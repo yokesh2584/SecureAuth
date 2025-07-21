e95d9db50a64becb606445e72c331118c7cb830d
Yokesh <yokeshananthan@gmail.com>
2025-07-21 09:29:28 +0530

    storage updated

--- client/src/stores/authStore.ts
+++ client/src/stores/authStore.ts
@@ -1,5 +1,5 @@
 import { create } from "zustand";
-import { persist } from "zustand/middleware";
+import { createJSONStorage, persist } from "zustand/middleware";
 import axiosPrivate from "../utils/axios";
 import { toast } from "sonner";
 import {
@@ -139,6 +139,7 @@ export const useAuthStore = create<AuthStore>()(
     }),
     {
       name: "Auth-Storage",
+      storage: createJSONStorage(() => localStorage),
     },
   ),
 );
